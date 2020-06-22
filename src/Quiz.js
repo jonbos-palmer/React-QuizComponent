c09a7f6d7159b4a0305d529b2964fa1fd3f22d12
jonbos-palmer <jbos@thepalmergroup.com>
2020-06-22 09:48:32 -0500

    quiz component renders from JSON

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import "./App.css";
+let quizData = require("./quiz_data.json");
 
 class Quiz extends Component {
   constructor(props) {
     super(props);
+    this.state = { quiz_position: 1 };
   }
   render() {
-    return <div className="QuizQuestion">Quiz</div>;
+    return (
+      <div className="QuizQuestion">
+        {quizData.quiz_questions[0].instruction_text}
+      </div>
+    );
   }
 }
 
