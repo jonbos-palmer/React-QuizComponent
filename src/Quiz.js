4281ed569735f424c45318af64e18f4393490316
jonbos-palmer <jbos@thepalmergroup.com>
2020-06-22 09:30:28 -0500

    test: mounts quiz component

---
+++ src/Quiz.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class Quiz extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div className="QuizQuestion">Quiz</div>;
+  }
+}
+
+export default Quiz;
